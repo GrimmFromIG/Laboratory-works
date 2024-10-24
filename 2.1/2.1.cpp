ab5e98b2d28352a293ae33b82588a6c2fa6cad9f
Bogdan <omnirock698@gmail.com>
2024-10-24 19:54:32 +0300

    с

--- 2.1/2.1.cpp
+++ 2.1/2.1.cpp
@@ -12,7 +12,7 @@
 int main()
 {
 	float Array1[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	for (int i = 0; i < sizeof(Array1) / 4; i++)
+	for (int i = 0; i < sizeof(Array1) / sizeof(Array1[0]); i++)
 	{
 		if (i % 2 != 0)
 		{
@@ -23,18 +23,17 @@ int main()
 			Array1[i] = 15.68 + 3 * i;
 		}
 	}
-	for (int i = 0; i < sizeof(Array1) - 1; i++)
+	for (int i = 0; i < sizeof(Array1)/ sizeof(Array1[1]) - 1; i++)
 	{
-		for (int j = 0; j < sizeof(Array1) - i - 1; j++)
+		for (int j = 0; j < sizeof(Array1)/ sizeof(Array1[1]) - i - 1; j++)
 		{
 			if (Array1[j] > Array1[j + 1])
 			{
-			int temp = Array1[j];
+			float temp = Array1[j];
 			Array1[j] = Array1[j + 1];
 			Array1[j + 1] = temp;
 			}
 		}
 	}
-
 	return 0;
 }
