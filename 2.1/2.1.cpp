15310a8db4bd666739c290a5d8ede4240106c661
Bogdan <omnirock698@gmail.com>
2024-10-24 19:28:06 +0300

    c

---
+++ 2.1/2.1.cpp
@@ -0,0 +1,40 @@
+﻿/*
+* Done by
+* Student name : Bohdan Petroshchuk
+* Student group : 121
+* Variant : 12
+* Lab. 2.1
+*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	float Array1[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	for (int i = 0; i < sizeof(Array1) / 4; i++)
+	{
+		if (i % 2 != 0)
+		{
+			Array1[i] = 15.68 - 3 * i;
+		}
+		else if (i % 2 == 0)
+		{
+			Array1[i] = 15.68 + 3 * i;
+		}
+	}
+	for (int i = 0; i < sizeof(Array1) - 1; i++)
+	{
+		for (int j = 0; j < sizeof(Array1) - i - 1; j++)
+		{
+			if (Array1[j] > Array1[j + 1])
+			{
+			int temp = Array1[j];
+			Array1[j] = Array1[j + 1];
+			Array1[j + 1] = temp;
+			}
+		}
+	}
+
+	return 0;
+}
