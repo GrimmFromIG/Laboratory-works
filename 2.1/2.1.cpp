9673d4fb3eafce4dfd4cb5e0ed89f792e352fe6f
Bogdan <omnirock698@gmail.com>
2024-10-24 20:27:51 +0300

    с

--- 2.1/2.1.cpp
+++ 2.1/2.1.cpp
@@ -11,29 +11,63 @@
 
 int main()
 {
-	float Array1[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	for (int i = 0; i < sizeof(Array1) / sizeof(Array1[0]); i++)
+	//Завдання 1
+	float FloatArray1[10] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	for (int i = 0; i < sizeof(FloatArray1) / sizeof(FloatArray1[0]); i++)
 	{
 		if (i % 2 != 0)
 		{
-			Array1[i] = 15.68 - 3 * i;
+			FloatArray1[i] = 15.68 - 3 * i;
 		}
 		else if (i % 2 == 0)
 		{
-			Array1[i] = 15.68 + 3 * i;
+			FloatArray1[i] = 15.68 + 3 * i;
 		}
 	}
-	for (int i = 0; i < sizeof(Array1)/ sizeof(Array1[1]) - 1; i++)
+	for (int i = 0; i < sizeof(FloatArray1) / sizeof(FloatArray1[1]) - 1; i++)
 	{
-		for (int j = 0; j < sizeof(Array1)/ sizeof(Array1[1]) - i - 1; j++)
+		for (int j = 0; j < sizeof(FloatArray1) / sizeof(FloatArray1[1]) - i - 1; j++)
 		{
-			if (Array1[j] > Array1[j + 1])
+			if (FloatArray1[j] < FloatArray1[j + 1])
 			{
-			float temp = Array1[j];
-			Array1[j] = Array1[j + 1];
-			Array1[j + 1] = temp;
+				float temp = FloatArray1[j];
+				FloatArray1[j] = FloatArray1[j + 1];
+				FloatArray1[j + 1] = temp;
 			}
 		}
 	}
+	//Завдання 2
+	int IntArray1[10];
+	int IntArray2[10];
+	int IntArray3[10];
+
+	for (int i = 0; i < sizeof(IntArray1) / sizeof(IntArray1[0]); i++)
+	{
+		IntArray1[i] = 95 + i;
+	}
+	for (int i = 0; i < sizeof(IntArray2) / sizeof(IntArray2[0]); i++)
+	{
+		IntArray2[i] = 105 - i;
+	}
+	int k = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (IntArray1[i] == IntArray2[j])
+			{
+				IntArray3[k] = IntArray1[i];
+				k++;
+				break;
+			}
+		}
+	}
+	int SumOfArray3 = 0;
+	for (int i = 0; i < k; i++)
+	{
+		if (IntArray3[i] > 101)
+		SumOfArray3 += IntArray3[i];
+	}
+
 	return 0;
 }
