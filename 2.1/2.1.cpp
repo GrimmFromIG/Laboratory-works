8b4d21425a9f9d89ee35cf909764130647bb0ded
Bogdan <omnirock698@gmail.com>
2024-10-25 08:42:15 +0300

    c

--- 2.1/2.1.cpp
+++ 2.1/2.1.cpp
@@ -12,6 +12,7 @@
 int main()
 {
 	//Завдання 1
+	std::cout << "Task 1" << std::endl;
 	float FloatArray1[10];
 	for (int i = 0; i < sizeof(FloatArray1) / sizeof(FloatArray1[0]); i++)
 	{
@@ -37,6 +38,7 @@ int main()
 		}
 	}
 	//Завдання 2
+	std::cout << "Task 2" << std::endl;
 	int IntArray1[10];
 	int IntArray2[10];
 	int IntArray3[10];
@@ -66,8 +68,38 @@ int main()
 	for (int i = 0; i < k; i++)
 	{
 		if (IntArray3[i] > 101)
-		SumOfArray3 += IntArray3[i];
+			SumOfArray3 += IntArray3[i];
 	}
+	//Завдання 3
+	std::cout << "Task 3" << std::endl;
+	float FloatArray2[5][4] =
+	{
+		{0, 11, -4, -76},
+		{-37, 72, -988, 88},
+		{12, 83, -96, 13},
+		{12, -33, -2, 45},
+		{12, 12, -232, -23}
+	};
+	int IntArray4[4];
+	for (int i = 0; i < 4; i++)
+	{
+		float mult = 1;
+		for (int j = 0; j < 5; j++)
+		{
+			if (FloatArray2[j][i] < 0)
+			{
+				mult = mult * FloatArray2[j][i];
+			}
+		}
+		IntArray4[i] = mult;
+	}
+	int kg = 0;
+	while (kg < sizeof(IntArray4)/sizeof(IntArray4[0]))
+	{
+		std::cout << IntArray4[kg] << " ";
+		kg++;
+	}
+	int IntArray5[5][5];
 
 	return 0;
 }
