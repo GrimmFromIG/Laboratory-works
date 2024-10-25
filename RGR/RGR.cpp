894c20d06092e2e45785f0bbcaaed9b566f583dc
Bogdan <omnirock698@gmail.com>
2024-10-25 11:03:14 +0300

    с

---
+++ RGR/RGR.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+
+bool isValidLV(const std::string& str) 
+{
+    const std::regex pattern(R"(\$[A-F0-9]+:\$[A-F0-9]+)");
+    return std::regex_match(str, pattern);
+}
+
+int main()
+{
+    std::string testStrings[] = { "$CDF:$567", "$CDF:$AAA", "$CDF:$A88", "$:SAAA" };
+
+    for (const auto& str : testStrings) 
+    {
+        if (isValidLV(str)) 
+        {
+            std::cout << str << " ∈ L(V)" << std::endl;
+        }
+        else 
+        {
+            std::cout << str << " ∉ L(V)" << std::endl;
+        }
+    }
+    return 0;
+}
