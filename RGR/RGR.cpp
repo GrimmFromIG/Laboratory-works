be6ad3c5e8f1eb03a1e19cb50ecbcb7752a0a665
Bogdan <omnirock698@gmail.com>
2024-10-25 13:37:12 +0300

    c

--- RGR/RGR.cpp
+++ RGR/RGR.cpp
@@ -1,26 +1,35 @@
-﻿#include <iostream>
+﻿/*
+* Done by
+* Student name : Bohdan Petroshchuk
+* Student group : 121
+* Variant : 21
+* RGR
+*/
+
+#include <Windows.h>
+#include <iostream>
 #include <string>
 #include <regex>
 
 bool isValidLV(const std::string& str) 
 {
-    const std::regex pattern(R"(\$[A-F0-9]+:\$[A-F0-9]+)");
+    // Regular expression pattern for the language L(V)
+    const std::regex pattern(R"(0[0-9]*![0-9]*1)");
     return std::regex_match(str, pattern);
 }
 
-int main()
-{
-    std::string testStrings[] = { "$CDF:$567", "$CDF:$AAA", "$CDF:$A88", "$:SAAA" };
-    for (const auto& str : testStrings) 
+int main() {
+    char16_t elementOf = u'\u2208';
+    std::string testStrings[] = { "0567!378781", "0!1", "0567378781", "0567!37878" };
+    for (const auto& str : testStrings)
     {
-        if (isValidLV(str)) 
-        {
-            std::cout << str << " ∈ L(V)" << std::endl;
+        if (isValidLV(str)) {
+            std::cout << str << " are element L(V)" << std::endl;
         }
-        else 
-        {
-            std::cout << str << " ∉ L(V)" << std::endl;
+        else {
+            std::cout << str << " are not element of L(V)" << std::endl;
         }
     }
+
     return 0;
 }
