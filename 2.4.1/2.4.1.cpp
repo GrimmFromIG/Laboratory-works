b135983796e03c99a67aa3dcb7e280e9eb6537cf
Bogdan <omnirock698@gmail.com>
2024-10-27 13:02:07 +0200

    c

---
+++ 2.4.1/2.4.1.cpp
@@ -0,0 +1,18 @@
+﻿/*
+* Done by
+* Student name : Bohdan Petroshchuk
+* Student group : 121
+* Variant : 12
+* Lab. 2.4.Task1
+*/
+
+#include <iostream>
+#include <locale>
+#include <Windows.h>
+
+int main()
+{
+    setlocale(LC_CTYPE, "ukr"); SetConsoleOutputCP(1251);
+
+    return 0;
+}
