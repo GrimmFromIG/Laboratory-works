57e247ca419e64ae88acfd750267e07fc6cddf1b
Bogdan <omnirock698@gmail.com>
2024-10-27 13:20:27 +0200

    с

--- 2.4/2.4.cpp
+++ 2.4/2.4.cpp
@@ -3,24 +3,23 @@
 * Student name : Bohdan Petroshchuk
 * Student group : 121
 * Variant : 12
-* Lab. 2.4.Task1
+* Lab. 2.4
 */
 
 #include <iostream>
 #include <locale>
 #include <Windows.h>
 #include <cmath>
-#include <stdexcept>
 
-double calculateY(double x) 
+double calculateY(double x)
 {
-    if (x < 0) 
+    if (x < 0)
     {
         return 4 * std::pow(x, 2) + 2;
     }
     else {
         double prod = 1.0;
-        for (int i = 1; i <= 5; ++i) 
+        for (int i = 1; i <= 5; ++i)
         {
             prod *= std::pow(x, 2) / (i + 1);
         }
@@ -28,14 +27,14 @@ double calculateY(double x)
     }
 }
 
-int main() 
+int main()
 {
     setlocale(LC_CTYPE, "ukr"); SetConsoleOutputCP(1251);
 
     double x;
     std::cout << "Enter x value: ";
     std::cin >> x;
-    while (x < -5 || x > 4) 
+    while (x < -5 || x > 4)
     {
         std::cout << "x must be in range of [-5, 4], please enter valid x value:" << std::endl;
         std::cin >> x;
