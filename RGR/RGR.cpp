e54897b72b12327a17dae973d009a01da4122236
Bogdan <omnirock698@gmail.com>
2024-10-28 10:42:28 +0200

    c

--- RGR/RGR.cpp
+++ RGR/RGR.cpp
@@ -9,26 +9,47 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
-#include <regex>
 
-bool isValidLV(const std::string& str) 
+bool isValidLV(const std::string& str)
 {
-    const std::regex pattern(R"(0[0-9]*![0-9]*1)");
-    return std::regex_match(str, pattern);
-}
-
-int main() {
-    char16_t elementOf = u'\u2208';
-    std::string testStrings[] = { "0567!378781", "0!1", "0567378781", "0567!37878" };
-    for (const auto& str : testStrings)
+    if (str.empty() || str.front() != '0' || str.back() != '1')
     {
-        if (isValidLV(str)) {
-            std::cout << str << " are element of L(V)" << std::endl;
-        }
-        else {
-            std::cout << str << " are not element of L(V)" << std::endl;
+        return false;
+    }
+    int ExclamationSignPos = str.find('!');
+    if (ExclamationSignPos == std::string::npos)
+    {
+        return false;
+    }
+    for (int i = 0; i < ExclamationSignPos; ++i)
+    {
+        if (!isdigit(str[i]))
+        {
+            return false;
         }
     }
+    for (int i = ExclamationSignPos + 1; i < str.length(); ++i) {
+        if (!isdigit(str[i]))
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
+int main() 
+{
+    while (true) 
+    {
+        std::string UserLine;
+        std::cout << "Enter the line: ";
+        std::getline(std::cin, UserLine);
+        if (isValidLV(UserLine)) {
+            std::cout << UserLine << " are element of L(V)" << std::endl;
+        }
+        else {
+            std::cout << UserLine << " are not element of L(V)" << std::endl;
+        }
+    }
     return 0;
 }
