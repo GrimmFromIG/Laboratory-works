0ac647c3d92d660feefb8a811883df86a2d40d03
Bogdan <omnirock698@gmail.com>
2024-10-31 09:15:39 +0200

    c

---
+++ 2.5/2.5.cpp
@@ -0,0 +1,16 @@
+﻿/*
+* Done by
+* Student name : Bohdan Petroshchuk
+* Student group : 121
+* Variant : 12
+* Lab. 2.5
+*/
+
+#include <iostream>
+#include <list.h>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
